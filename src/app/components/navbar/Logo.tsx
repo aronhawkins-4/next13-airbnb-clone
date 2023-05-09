df3a85811d50823c4b1b083de941f8e61c803296
Aron Hawkins <aron@sidekick.agency>
2023-05-09 16:30:40 -0500

    initial commit

---
+++ src/app/components/navbar/Logo.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '/public/images/logo.png';
+
+export const Logo = () => {
+	return (
+		<Link href='/'>
+			<Image
+				alt='Logo'
+				className='hidden md:block cursor-pointer'
+				height='100'
+				width='100'
+				src={logo}
+			></Image>
+		</Link>
+	);
+};
---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Logo } from './Logo';
+import { Container } from '../Container';
+import { Search } from './Search';
+import { UserMenu } from './UserMenu';
+export default function Navbar() {
+	return (
+		<div className='fixed w-full bg-white z-10 shadow-sm'>
+			<div className='py-4 border-b'>
+				<Container>
+					<div className='flex flex-row items-center justify-between gap-4 md:gap-0'>
+						<Logo />
+						<Search />
+						<UserMenu />
+					</div>
+				</Container>
+			</div>
+		</div>
+	);
+}
